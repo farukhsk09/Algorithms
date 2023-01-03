2782c50bb652d946f86b4f01d8b3f376ecbdc917
farukhsk09 <farukh.sk09@gmail.com>
2023-01-03 21:09:26 +0530

    Array doubles . Leetcode : 954

---
+++ arrays/array_doubles.py
@@ -0,0 +1,52 @@
+# 954. Array of Doubled Pairs
+
+class Solution:
+    def canReorderDoubled(self, arr: List[int]) -> bool:
+        arr.sort()
+        # print(arr)
+        newMap={}
+        for i in arr:
+            if i not in newMap:
+                newMap[i]=1
+                continue
+            newMap[i] +=1
+        # print(newMap)
+        for i in arr:
+            if i < 0: 
+                # [-8,-4,-4,-2]
+                # check its still valid 
+                if i in newMap:
+                    if i/2 in newMap:
+                        newMap[i/2]-=1
+                        if newMap[i/2] == 0:
+                            newMap.pop(i/2)
+                    else:
+                        return False
+                    newMap[i]-=1
+                    if newMap[i] == 0:
+                        newMap.pop(i)
+                else:
+                    continue
+            elif i > 0:
+                # [2,4,4,8]
+                if i in newMap:
+                    if i*2 in newMap:
+                        newMap[i*2]-=1
+                        if newMap[i*2] == 0:
+                            newMap.pop(i*2)
+                    else:
+                        return False
+                    newMap[i]-=1
+                    if newMap[i] == 0:
+                        newMap.pop(i)
+                else:
+                    continue
+                    
+                # check its half presense  
+            else:
+                if newMap[i]%2 == 0:
+                    continue
+                else:
+                    return False 
+
+        return True
